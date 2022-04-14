31cca0a990dada13cf646203e907efbc2b9267ba
Nikita Kuznetsov <nikuznetsoff@gmail.com>
2022-04-14 23:15:13 +0300

    Bug fix, minor changes
    I commented some parts that were in the initial code and replaced them with ones that more suitable from my point of view. Also, appeared bug is fixed and dot ptoduct is provided correctly.
    #pragma omp parallel shared(sum) removed to the fuction because we want to parallize the loop, so it is needed to be placed with the loop.

--- HW2/BugReduction.c
+++ HW2/BugReduction.c
@@ -4,38 +4,42 @@
 
 float dotprod(float * a, float * b, size_t N)
 {
-    int i, tid;
+    //int i, tid;
     float sum;
 
-    tid = omp_get_thread_num();
-
-#pragma omp for reduction(+:sum)
-    for (i = 0; i < N; ++i)
+    //tid = omp_get_thread_num();
+    
+    #pragma omp parallel shared(sum)
     {
-        sum += a[i] * b[i];
-        printf("tid = %d i = %d\n", tid, i);
-    }
+        int tid = omp_get_thread_num();
 
+        #pragma omp for reduction(+:sum)
+        for (int i = 0; i < N; ++i)
+        {
+            sum += a[i] * b[i];
+            printf("tid = %d i = %d\n", tid, i);
+        }
+    }
     return sum;
 }
 
 int main (int argc, char *argv[])
 {
     const size_t N = 100;
-    int i;
+    //int i;
     float sum;
     float a[N], b[N];
-
-
-    for (i = 0; i < N; ++i)
+    
+    for (int i = 0; i < N; ++i)
     {
         a[i] = b[i] = (double)i;
     }
 
-    sum = 0.0;
+    //sum = 0.0;
 
-#pragma omp parallel shared(sum)
-    dotprod(a, b, N);
+//#pragma omp parallel shared(sum)
+    //dotprod(a, b, N);
+    sum = dotprod(a, b, N);
 
     printf("Sum = %f\n",sum);
 
