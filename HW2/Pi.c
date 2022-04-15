8f6b120faf9f6cad2ae4603a779c98c73ab61eb9
Nikita Kuznetsov <nikuznetsoff@gmail.com>
2022-04-15 23:59:46 +0300

    First attempt
    Seed needs to be fixed

--- HW2/Pi.c
+++ HW2/Pi.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
 
-int main()
+#define N_THREADS 2
+
+int main(int argc, char **argv)
 {
     const size_t N = 100000;
-    double step;
 
-    double x, pi, sum = 0.;
+    double pi;
+    int inside_points = 0;
+    
+    double start, end;
 
-    step = 1. / (double)N;
+    omp_set_num_threads(N_THREADS);
 
-    for (int i = 0; i < N; ++i)
+    start = omp_get_wtime();
+
+    #pragma omp parallel shared(inside_points)
     {
-        x = (i + 0.5) * step;
-        sum += 4.0 / (1. + x * x);
+        int tid = omp_get_thread_num();	
+	unsigned int x_seed = 2314343223 * (tid + 1);
+	unsigned int y_seed = 4565463432 * (tid + 1);
+
+	double x, y;
+
+        #pragma omp for reduction(+:inside_points) private(x, y)
+	for (int i = 0; i < N; ++i)
+	{
+	    x = ((double) rand_r(&x_seed)) / (double) RAND_MAX;
+	    y = ((double) rand_r(&y_seed)) / (double) RAND_MAX;
+	    if (x*x + y*y < 1) ++inside_points;
+	}        
     }
 
-    pi = step * sum;
+    end = omp_get_wtime();
 
-    printf("pi = %.16f\n", pi);
+    printf("pi = %.16f\n", 4 * (double)inside_points / N);
+    printf("Time elapsed: %.16f seconds\n", (double)(end - start));
 
     return 0;
 }
