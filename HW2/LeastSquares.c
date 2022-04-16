ea4333d470420fcbbc47c92047e86f3aeee67649
Nikita Kuznetsov <nikuznetsoff@gmail.com>
2022-04-16 23:45:12 +0300

    Working version

--- HW2/LeastSquares.c
+++ HW2/LeastSquares.c
@@ -6,31 +6,32 @@
 
 #define N_THREADS 4
 #define N 100
-#define LR 0.00001
+#define LR 0.1
 #define EPOCHS 5000
 
 int main()
 {     
-    double * x, * y, * y_pred;
-    double a, b, a_found = 0, b_found = 0;
-    double start, end, eps, error, d_da = 0, d_db = 0;
+    double * x, * y;
+    double a, b, a_found = 0, b_found = 0, y_pred, err_tmp;
+    double start, end, eps, error = 0, d_da = 0, d_db = 0;
 
     x = (double *) malloc(N * sizeof(double));
     y = (double *) malloc(N * sizeof(double));
     
-    y_pred = (double *) malloc(N * sizeof(double));
+    //y_pred = (double *) malloc(N * sizeof(double));
 
     srand(time(NULL));  
     
-    a = -15.0 + ( (double)rand() / ((double)RAND_MAX / 30.0) ); 
-    b = -15.0 + ( (double)rand() / ((double)RAND_MAX / 30.0) );
+    a = -2.0 + ( (double)rand() / ((double)RAND_MAX / 4.0) ); 
+    b = -3.0 + ( (double)rand() / ((double)RAND_MAX / 6.0) );
 
     //omp_set_num_threads(N_THREADS);
     //#pragma omp parallel for shared(x, y, a, b, eps)
     for (int i = 0; i < N; ++i)
     {   
-        x[i] = 15.0 + ( (double)rand() / ((double)RAND_MAX / 85.0) );
+        x[i] = 0.0 + ( (double)rand() / ((double)RAND_MAX / 2.0) );
 	eps = -0.1 + ( (double)rand() / ((double)RAND_MAX / 0.2) );
+	//printf("%f\n", eps);
         y[i] = a * x[i] + b + eps;
     }
 
@@ -38,12 +39,12 @@ int main()
     //#pragma omp parallel for
     for (int epoch = 0; epoch < EPOCHS; ++epoch)
     {
-        for (int i = 0; i < N; ++i) y_pred[i] = a_found * x[i] + b_found;
-
-	for (int i = 0; i < N; ++i)
+        for (int i = 0; i < N; ++i) 
 	{
-	    d_da = d_da + x[i] * (y[i] - y_pred[i]);
-	    d_db = d_db + (y[i] - y_pred[i]);
+	    y_pred = a_found * x[i] + b_found;
+
+	    d_da = d_da + x[i] * (y[i] - y_pred);
+	    d_db = d_db + (y[i] - y_pred);
 	}
 
 	d_da = (-2.0 / (double)N) * d_da;
@@ -53,9 +54,25 @@ int main()
 	b_found = b_found - (double)LR * d_db;
     }
     end = omp_get_wtime();
+    
+    printf("-----------------\n");
+    printf("   X    |   Y\n");
+    printf("-----------------\n");
+    for (int i = 0; i < N; ++i)
+    {
+       printf("%.5f | %.5f\n", x[i], y[i]);
+    }
 
-    printf("Real parameters: a = %.5f b = %.5f\n", a, b);
+    for (int i = 0; i < N; ++i)
+    {
+	err_tmp = y[i] - (a_found * x[i] + b_found);
+        error = error + err_tmp * err_tmp;
+    }
+    error = error / (double)N; 
+
+    printf("\nReal parameters: a = %.5f b = %.5f\n", a, b);
     printf("Found parameters: a = %.5f b = %.5f\n", a_found, b_found);
-    printf("Elapsed time = %.5f seconds\n", (double)(end - start));  
+    printf("Sum of squared residuals = %.5f\n", error);
+    printf("\nElapsed time = %.5f seconds\n", (double)(end - start));  
     return 0;
 }
