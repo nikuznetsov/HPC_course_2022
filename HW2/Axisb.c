c2ddc57487f9727c78191dd262a38494188231ab
Nikita Kuznetsov <nikuznetsoff@gmail.com>
2022-04-18 19:10:51 +0300

    Draft version

--- HW2/Axisb.c
+++ HW2/Axisb.c
@@ -4,61 +4,97 @@
 #include <math.h>
 #include <time.h>
 
-#define N_THREADS 4
-#define TOL 0.0001
-#define N 3
-#define MAX_ITER 1000
+#define N_THREADS 4 
+#define TOL 0.000001
+#define N 2000
+#define MAX_ITER 5000
 
 int main()
-{
+{	
     srand(time(NULL));
 
     double ** A = (double **)calloc(N, sizeof(double*));
-    for (int i = 0; i < N; ++i)  A[i] = (double *)calloc(N, sizeof(double));  
+    
+    for (int i = 0; i < N; ++i)  A[i] = (double *)calloc(N, sizeof(double)); 
+
     for (int i = 0; i < N; ++i)
     {
-        for (int j = 0; j < N; ++j) A[i][j] = 1 + rand() % 10;
-	A[i][i] = N * 10 + rand() % 5;
+        for (int j = 0; j < N; ++j) 
+	{
+	    if (i != j) A[i][j] = ((double)rand() / (double)(RAND_MAX)) * 9.0;
+	    else A[i][i] = ((double)rand() / (double)RAND_MAX) * 100.0;
+	}
     }
 
     double * b = (double *)calloc(N, sizeof(double));
-    for (int i = 0; i < N; ++i) b[i] = rand() % 20;
+    
+    for (int i = 0; i < N; ++i) 
+    {
+	b[i] = ((double)rand() / (double)RAND_MAX) * 20.0;
+    }
 
     double * x = (double *)calloc(N, sizeof(double));
 
     double * dx = (double *)calloc(N, sizeof(double));
 
-    double delxi, numit = 0;
+    double start = omp_get_wtime();
 
     for (int k = 0; k < MAX_ITER; ++k)
     {
-        double norm_error = 0.0;
-	for (int i = 0; i < N; ++i)
-	{
-	    dx[i] = b[i];
-	    delxi = 0.0;
+        double error = 0.0, sigma;
 
+	#pragma omp parallel for shared(A, b, x, dx) \
+	   		         num_threads(N_THREADS) \
+	                         reduction(+:error, sigma) \
+	                         schedule(static)
+	for (int i = 0; i < N; ++i)
+	{ 
+	    dx[i] = b[i];
+	    sigma = 0.0;
+            
 	    for (int j = 0; j < N; ++j)
 	    {
-	        delxi = delxi + A[i][j] * x[j];
+	        sigma = sigma + A[i][j] * x[j];
 	    }
 
-	    dx[i] = dx[i] - delxi;
+	    dx[i] = dx[i] - sigma;
 	    dx[i] = dx[i] / A[i][i];
 	    x[i] = x[i] + dx[i];
-	    norm_error = norm_error + dx[i] * dx[i];
-	}
-	if (sqrt(norm_error) <= TOL) break;
+	    error = error + dx[i] * dx[i];
+        }
 
-        numit = k+1;
+	if (sqrt(error) <= TOL) break;
     }
 
-    free(dx);
+    double end = omp_get_wtime();
+    
+    /* Print the system and solution
+    printf("System of equations:\n");
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; j++) 
+	{
+	    if (j == 0) printf("| %2.f  ", A[i][j]);
+	    else if (j == N-1) printf("%2.f |  ", A[i][j]);
+	    else printf("%2.f  ", A[i][j]);
+	}
+	printf("| x%d |  =  | %2.f |\n", i+1, b[i]);
+    }
+
+    printf("\nSolution:\n");
 
     for (int i = 0; i < N; ++i)
     {
-        printf("x%i=%.3f\n", i+1, x[i]);
+        printf("x%i = %.3f\n", i+1, x[i]);
     }
+    */
+
+    printf("Time elapsed OpenMP (N_THREADS = %d) = %.5f seconds\n", N_THREADS, (double)(end - start));
+    
+    free(dx);
+    free(A);
+    free(b);
+    free(x);
 
     return 0;
 }
